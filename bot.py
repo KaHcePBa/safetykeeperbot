bf5f62b148867a9cf3a4b83bc3773fbe5ab4f843
KaHcePBa <godhead@tut.by>
2025-01-27 23:26:56 +0300

    feature wrapping shine

--- bot.py
+++ bot.py
@@ -9,25 +9,22 @@ from config.config import settings  # Импорт настроек из dynacon
 dynaconf_settings = settings
 
 
-def get_analyze_url(users_url):
+def get_analyze_url(url_to_analyze):
     """
     Выполняет запрос на анализ URL и возвращает значение links.self из ответа.
-
-    :param users_url: URL для анализа
+    :param url_to_analyze: URL для анализа
     :return: Ссылка links.self из ответа API
     """
     virustotal_url = dynaconf_settings.VT_URL_START
-    payload = {'url': users_url}  # Параметры запроса
+    payload = {'url': url_to_analyze}  # Параметры запроса
     headers = {
         'accept': 'application/json',
         'x-apikey': dynaconf_settings.VIRUSTOTAL_APIKEY,
         'content-type': 'application/x-www-form-urlencoded'
     }
 
-    # Выполнение POST-запроса
     response = requests.post(virustotal_url, data=payload, headers=headers)
 
-    # Проверка статуса ответа
     if response.status_code != 200:
         raise Exception(f"Ошибка API: {response.status_code}, {response.text}")
 
@@ -44,8 +41,7 @@ def get_analyze_url(users_url):
 
 def get_report(links_self):
     """
-    Получает детальный отчёт по ссылке links.self из API VirusTotal.
-
+    Получает детальный отчёт о результатах проверки по ссылке links.self из API VirusTotal.
     :param links_self: Ссылка links.self для получения отчёта
     """
     headers = {
@@ -53,15 +49,13 @@ def get_report(links_self):
         'x-apikey': dynaconf_settings.VIRUSTOTAL_APIKEY
     }
 
-    # Выполнение GET-запроса
     response = requests.get(links_self, headers=headers)
 
-    # Проверка успешности запроса
     if response.status_code == 200:
         parsed_json = response.json()
         attributes = parsed_json.get('data', {}).get('attributes', {})
 
-        # Извлечение интересующих данных
+        # Извлечение интересующих данных из ответа JSON
         filtered_attributes = {
             'status': attributes.get('status'),
             'stats': attributes.get('stats')
@@ -80,25 +74,25 @@ def get_report(links_self):
             print(f'code: {error_code}')
             print(f"message: {error_message}")
         except json.JSONDecodeError:
-            # Если тело ответа не в формате JSON
+            # Если тело ответа не в формате JSON возвращаю ошибку
             print(f"HTTP: {response.status_code}")
             print("Error: Unable to decode JSON response.")
             print(f"Raw response: {response.text}")
 
 
 if __name__ == "__main__":
-    # URL для анализа
-    url_to_analyze = 'https://click.ru/'
+    # URL пользователя для анализа. Сюда передавать значение из message пользователя Telegram
+    users_url = 'https://click.ru/'
 
     try:
-        # Сначала получаем links.self
-        links_self = get_analyze_url(url_to_analyze)
+        # Сначала получаю links.self (ссылка на результаты проверки) из функции get_analyze_url
+        links_self = get_analyze_url(users_url)
 
-        # Задержка 5 секунд
+        # Задержка 5 секунд для прохождения проверки
         print("Ожидание 5 секунд перед получением отчёта...")
         time.sleep(5)
 
-        # Затем передаём его в get_report
+        # Передаю links.self в get_report для выгрузки результата проверки
         get_report(links_self)
     except Exception as e:
         print(f"Ошибка: {e}")
